3448b2f5bfc71a995edb2bba1ce0c6ffec6c4dcb
jade <mayuhome@163.com>
2023-08-01 15:41:40 +0800

    add question to page

---
+++ lib/data/questions.dart
@@ -0,0 +1,10 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('questions1?', ['answer1-1','answer1-2','answer1-3','answer1-4']),
+  QuizQuestion('questions2?', ['answer2-1','answer2-2','answer2-3','answer2-4']),
+  QuizQuestion('questions3?', ['answer3-1','answer3-2','answer3-3','answer3-4']),
+  QuizQuestion('questions4?', ['answer4-1','answer4-2','answer4-3','answer4-4']),
+  QuizQuestion('questions5?', ['answer5-1','answer5-2','answer5-3','answer5-4']),
+  QuizQuestion('questions6?', ['answer6-1','answer6-2','answer6-3','answer6-4']),
+];
